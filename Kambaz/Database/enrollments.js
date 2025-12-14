1b47bbfeaac937f317902e179da20ff18d250c89
Drumil23 <drumil.kotecha@gmail.com>
2025-12-14 14:44:13 -0500

    refactor enrollments data: update user IDs for consistency and clarity in the enrollments dataset

--- Kambaz/Database/enrollments.js
+++ Kambaz/Database/enrollments.js
@@ -1,31 +1,31 @@
 export default [
     {
-        "_id": "1",
+        "_id": "u3",
         "user": "123",
         "course": "RS101"
     },
     {
-        "_id": "2",
+        "_id": "u2",
         "user": "234",
         "course": "RS101"
     },
     {
-        "_id": "3",
+        "_id": "u3",
         "user": "345",
         "course": "RS101"
     },
     {
-        "_id": "4",
+        "_id": "u4",
         "user": "456",
         "course": "RS101"
     },
     {
-        "_id": "5",
+        "_id": "u5",
         "user": "567",
         "course": "RS101"
     },
     {
-        "_id": "6",
+        "_id": "u6",
         "user": "234",
         "course": "RS102"
     },
@@ -35,12 +35,12 @@ export default [
         "course": "RS102"
     },
     {
-        "_id": "8",
+        "_id": "u1",
         "user": "890",
         "course": "RS102"
     },
     {
-        "_id": "9",
+        "_id": "u6",
         "user": "123",
         "course": "RS102"
     }
